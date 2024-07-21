46c128720c516a4c85a20a0da6227ca8b04ea047
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-07-21 20:59:38 +0300

    create logic to mark buttons
    create logic to mark set buttons in home page for search

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./searchBar.scss";
 
+
+const types = ["buy", "rent"];
+
+
 const SearchBar = () => {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location:  "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (value) => {
+    // update "type" by selected button (buy or rent)
+    setQuery((prev) => ({...prev, type: value}));
+  };
+
   return (
-    <div>SearchBar</div>
+    <div className='searchBar'>
+      <div className="type">
+        {types.map((type) => ( 
+        <button 
+          key={type}     
+          onClick={() => switchType(type)}
+          className={query.type === type ? "active" : ""}
+        >{type}</button>
+        ))}
+      </div>
+      <form>
+        <input type="text" name="location" placeholder='City Location...?' value=""/>
+        <input type="number" name="minPrice" min={0} max={10000000} placeholder='Min Price?' value=""/>
+        <input type="number" name="maxPrice" min={0} max={10000000} placeholder='Max Price?' value=""/>
+      </form>
+      <button>
+        <img src="./search.png" alt="search-icon"/>
+      </button>
+    </div>
   )
 }
 
-export default SearchBar
+export default SearchBar;
