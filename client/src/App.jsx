d285c1deee6a4a3806b4df35a9728b9cf64e73c2
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-08-07 18:37:38 +0300

    instal react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,35 @@
 import './layout.scss';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/homePage/HomePage';
+import listPage from './pages/listPage/listPage';
+
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage/>,
+    },
+    {
+      path: "/list",
+      element: <listPage/>,
+    },
+  ]);
+
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar/>
-      </div>
-      <div className="content">
-        <HomePage/>
-      </div>
-    </div>
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar/>
+    //   </div>
+    //   <div className="content">
+    //     <HomePage/>
+    //   </div>
+    // </div>
+    <RouterProvider router={router}/>
   );
 }
 
 export default App;
-
-
