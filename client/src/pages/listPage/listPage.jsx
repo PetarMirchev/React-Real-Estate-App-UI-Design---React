ca772a43d730193dc9f75324c722d37fadada53f
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-08-10 11:25:20 +0300

    create dummy data and start list page
    start create list page & add dummy data for this page

--- client/src/pages/listPage/listPage.jsx
+++ client/src/pages/listPage/listPage.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import "./listPage.scss";
+import { listData } from '../../lib/dummyData';
+import Filter from '../../components/Filter/Filter';
+import Card from '../../components/Card/Card';
 
 const ListPage = () => {
+
+  const data = listData;
+
   return (
-    <div>
-      <h1>listPage</h1>
+    <div className='listPage'>
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter/>
+          {data.map(item => (
+            <Card key={item.id} item={item}/>
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>
     </div>
   )
 }
