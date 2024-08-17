53c0fdeceb095de6c6a945de3a701c294162049d
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-08-17 17:37:48 +0300

    add map whit shown locations

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import './map.scss';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer} from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
+import Pin from '../Pin/Pin';
 
 
 
-const Map = () => {
+const Map = ({items}) => {
 
   return (
     // !import to not be put in <div> box to work!!!!
-         <MapContainer center={[52.4797, -1.90269]} zoom={13} scrollWheelZoom={false} className='map'>
+         <MapContainer center={[30.2672, -97.7431]} zoom={4} scrollWheelZoom={false} className='map'>
             <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[52.4797, -1.90269]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+          {items.map( (item) => (
+            <Pin item={item} key={item.id} />
+          ))}
         </MapContainer>
   )
 }
--- client/src/pages/listPage/listPage.jsx
+++ client/src/pages/listPage/listPage.jsx
@@ -19,7 +19,7 @@ const ListPage = () => {
         </div>
       </div>
       <div className="mapContainer">
-        <Map/>
+        <Map items={data}/>
       </div>
     </div>
   )
