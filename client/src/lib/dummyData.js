b025bd62807fddbc2894148a2a2c211426be3103
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-08-23 18:06:36 +0300

    add style & content to info part of page

--- client/src/lib/dummyData.js
+++ client/src/lib/dummyData.js
@@ -178,5 +178,5 @@ export const singlePostDummyData = {
   school: "250m away",
   bus: "100m away",
   restaurant: "75m away",
-  description: "Immerse yourself in unparalleled luxury, experience the epitome of urban living."
+  description: "Immerse yourself in unparalleled luxury, experience the epitome of urban living. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }
--- client/src/pages/singlePage/SinglePage.jsx
+++ client/src/pages/singlePage/SinglePage.jsx
@@ -21,11 +21,13 @@ const SinglePage = () => {
                   <div className="price">$ {singlePostDummyData.price}</div>
                 </div>
                 <div className="user">
-                  <img src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="user photo"/>
+                  <img src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="user Avatar"/>
                   <span>Pepi Random</span>
                 </div>
               </div>
-              <div className="bottom"></div>
+              <div className="bottom">
+                {singlePostDummyData.description}
+              </div>
             </div>
         </div>
       </div>
