46c4b8dbf1c1bfedabab155d4be5946ab6680864
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-09-06 10:45:02 +0300

    add content in user profile page
    add user info, add list of create user content, add list for save content

---
+++ client/src/components/List/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./list.scss";
+import Card from '../Card/Card';
+import { listData } from '../../lib/dummyData';
+
+
+const List = () => {
+  return (
+    <div className='list'>
+        {listData.map((item) => (
+           <Card key={item.id} item={item}/>
+        ))}
+    </div>
+  )
+}
+
+export default List
--- client/src/pages/profilePage/profilePage.jsx
+++ client/src/pages/profilePage/profilePage.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import "./profilePage.scss";
+import List from '../../components/List/List';
 
 
 const profilePage = () => {
   return (
     <div className='profilePage'>
         <div className="details">
-            <div className="wrapper"></div>
+            <div className="wrapper">
+                <div className="title">
+                    <h1>User Information</h1>
+                    <button>Update Profile</button>
+                </div>
+                <div className="info">
+                    <span>Avatar: <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /></span>
+                    <span>Username: <b>Petar Mir</b></span>
+                    <span>Email: <b>petar_g@abv.bg</b></span>
+                </div>
+                <div className="title">
+                    <h1>My List</h1>
+                    <button>Create New Post</button>
+                </div>
+                <List />
+                <div className="title">
+                    <h1>Save List</h1>
+                </div>
+                <List />
+            </div>
         </div>
         <div className="chatContainer">
             <div className="wrapper"></div>
